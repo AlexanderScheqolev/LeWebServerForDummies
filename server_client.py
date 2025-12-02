f527624b3be50b920920a0220ddc1b22daa03ed1
Александр <scetch2003@gmail.com>
2025-12-02 19:05:28 +0300

    Added Put Method

--- server_client.py
+++ server_client.py
@@ -1,11 +1,31 @@
 import socket
 import hashlib
-
+import json
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 s.bind(("localhost", 3030))
 s.listen(1)
 conn, addr = s.accept() # Принимаем разрешение на прием и передачу данных
+
+def Put(Source: str, Payload: str):
+    """
+    :param Source: Фамилия (строка)
+    :param ID: Я решил сдесь сделать sha256 айдишник (Взял строковый тип), ID == "RandomStuff" -> sha256()
+    :param Payload: Тело для сохранения в файл (Тоже строковый)
+    :return: DataFile: JSON-файл с пакетом
+    """
+    hash = hashlib.sha256()
+    ID = hash.update(b"RandomStuff")
+    ID = hash.hexdigest()
+    data = {
+        "Source": Source,
+        "ID": ID,
+        "Payload": Payload
+    }
+    json_str = json.dumps(data, indent=4)
+    with open("Data.json", "w") as DataFile:
+        DataFile.write(json_str)
+    return DataFile
 
 while True:
     data = conn.recv(1024)
